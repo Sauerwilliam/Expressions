d13d47543799ef397d7a325af91b2f2a382e304c
william <sauerwilliam196@gmail.com>
2020-09-14 15:54:49 -0600

    Finished equations for now

--- Expressions/src/com/william/expressions/Program.java
+++ Expressions/src/com/william/expressions/Program.java
@@ -8,16 +8,27 @@ public class Program {
 
 	public static void main(String[] args) throws IOException {
 		
+		while(true) {
+	        
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
         String numbers = reader.readLine();//"4x + 3 + 5";//System.in.();
         Equation input = EquationBuilder.buildEquation(numbers);
         
         
-        while(true) {
-        	String x = reader.readLine();//"4x + 3 + 5";//System.in.();
-        	System.out.println(new Equation( ExpressionBuilder.buildExpression(x.substring(0,x.indexOf('=') - 1)) ,ExpressionBuilder.buildExpression(x.substring(x.indexOf('=') + 1) )));
-        	
-        	
+        	//String x = reader.readLine();//"4x + 3 + 5";//System.in.();
+        	//int newX = Integer.parseInt(reader.readLine());
+            //System.out.println(input.isValid(newX));
+            boolean solutions = false;
+        	for(int i = -1000; i < 1000; i++) {
+            	
+            	if(input.isValid(i)) {
+            		solutions = true;
+            		System.out.println("" + i + " is a solution");
+            	}
+            }
+        	if(!solutions) {
+        		System.out.println("No whole number solutions between -1000 and 999.");
+        	}
         }
 //        while(true) {
 //        	String x = reader.readLine();//"4x + 3 + 5";//System.in.();
